3bc1928b02c45b782dba7e07efd9b0dd5c40adab
pawelkossak <michalpawel12345@gmail.com>
2024-10-03 22:53:19 +0200

    finished registration form and redirect to panel

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 import express, { response } from "express"
 import axios from "axios"
+import bodyParser from "body-parser"
 const app = express()
 const port = 3000
-var userAuth = "84a6b575123c3cd630db680bf2fc21f798c431680850b81fca5c95ddb4ab82e0e1afe9854a9a249d0da7a03c7a6589ec"
-var registered = false
-
-const API_URL = "https://api.foxypool.io/api/v3/chia-og"
+var isRegistered = false
+var API_URL = ""
+var userAuth = ""
 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
-    if (registered) {
-        res.redirect("/")
+    if (isRegistered) {
+        res.redirect("/panel")
     }
     else {
         res.render("registration.ejs")
     }
 })
 
+app.post('/register', (req, res) => {
+    if (isRegistered) {
+        res.redirect("/panel")
+    }
+    else {
+        if (req.body.pool == 'option-1'){
+            API_URL = "https://api.foxypool.io/api/v3/chia"
+        }
+        else if (req.body.pool == 'option-2'){
+            API_URL = "https://api.foxypool.io/api/v3/chia-og"
+        }
+        userAuth = req.body.publicKey
+        isRegistered = true
+        res.redirect("/panel")
+    }
+})
+
+
 app.get('/panel', (req, res) => {
+    if (isRegistered){
     axios.get(API_URL + `/account/${userAuth}`)
     .then((response) => {
         res.render("index.ejs", { 
@@ -27,8 +48,13 @@ app.get('/panel', (req, res) => {
         })
     })
     .catch((error) => {
-        res.render("index.ejs", { data: JSON.stringify(error.response.data), userAuth: userAuth })
+        res.render("index.ejs", { data: JSON.stringify(error.data), userAuth: userAuth })
     })
+    }
+    else {
+        res.redirect("/")
+    }
+
 })
 
 
